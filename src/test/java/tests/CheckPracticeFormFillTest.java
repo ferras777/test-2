b55fcb7167aa64d9f344b820b98fbbe0a94ab93e
Ilya_Malykh <ferras2007@yandex.ru>
2021-01-01 21:51:02 +0300

    add selenoid support

--- src/test/java/tests/CheckPracticeFormFillTest.java
+++ src/test/java/tests/CheckPracticeFormFillTest.java
@@ -1,34 +1,50 @@
 package tests;
 
+import com.github.javafaker.Faker;
+import com.github.javafaker.service.FakeValuesService;
+import com.github.javafaker.service.RandomService;
 import org.junit.jupiter.api.Test;
 import pages.PracticeFormPage;
 import users.User;
 
-public class CheckPracticeFormFillTest {
+import java.util.Locale;
 
-    User user = new User("James", "Dow", "test@test.com", "1234567890",
-            "Male", "Arts", "Sports", "NY Street", "Haryana", "Karnal"
-            , "25", "June", "1992");
+import static io.qameta.allure.Allure.step;
+
+public class CheckPracticeFormFillTest extends TestBase {
+
+    Faker faker = new Faker();
+    FakeValuesService fakeValuesService = new FakeValuesService(
+            new Locale("en-GB"), new RandomService());
+
+    User user = new User(faker.name().firstName(), faker.name().lastName(),
+            fakeValuesService.bothify("????##@gmail.com"), fakeValuesService.regexify("[0-9]{10}"),
+            "Male", "Arts", "Sports", faker.address().fullAddress(), "Haryana",
+            "Karnal", "25", "June", "1992");
 
     PracticeFormPage practiceFormPage = new PracticeFormPage(user);
 
     @Test
     public void checkPracticeFormFillTest() {
-        practiceFormPage.openPage();
-        practiceFormPage.fillFirstName();
-        practiceFormPage.fillLastName();
-        practiceFormPage.fillEmail();
-        practiceFormPage.selectGender();
-        practiceFormPage.fillMobileNumber();
-        practiceFormPage.selectDateOfBirth();
-        practiceFormPage.selectSubject();
-        practiceFormPage.selectHobby();
-        practiceFormPage.uploadPhoto();
-        practiceFormPage.fillCurrentAddress();
-        practiceFormPage.selectState();
-        practiceFormPage.selectCity();
-        practiceFormPage.clickSubmitButton();
 
-        practiceFormPage.checkSubmitForm();
+        step("Open students registration form", () -> practiceFormPage.openPage());
+
+        step("Fill submit form", () -> {
+            practiceFormPage.fillFirstName();
+            practiceFormPage.fillLastName();
+            practiceFormPage.fillEmail();
+            practiceFormPage.selectGender();
+            practiceFormPage.fillMobileNumber();
+            practiceFormPage.selectDateOfBirth();
+            practiceFormPage.selectSubject();
+            practiceFormPage.selectHobby();
+            practiceFormPage.uploadPhoto();
+            practiceFormPage.fillCurrentAddress();
+            practiceFormPage.selectState();
+            practiceFormPage.selectCity();
+            practiceFormPage.clickSubmitButton();
+        });
+
+        step("Check results", () -> practiceFormPage.checkSubmitForm());
     }
 }
